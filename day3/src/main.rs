d0ac32a49fe07960dfb02f561969d8f1dea4801b
Kai <kelwooddieu@mathematica-mpr.com>
2024-12-15 14:43:07 -0500

    added day3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,3 +1,149 @@
+use std::io::Read;
+use std::fs::File;
+use regex::Regex;
+
+// --- Day 3: Mull It Over ---
+// "Our computers are having issues, so I have no idea if we have any Chief Historians in stock! You're welcome to check the warehouse, though," says the mildly flustered shopkeeper at the North Pole Toboggan Rental Shop. The Historians head out to take a look.
+
+// The shopkeeper turns to you. "Any chance you can see why our computers are having issues again?"
+
+// The computer appears to be trying to run a program, but its memory (your puzzle input) is corrupted. All of the instructions have been jumbled up!
+
+// It seems like the goal of the program is just to multiply some numbers. It does that with instructions like mul(X,Y), where X and Y are each 1-3 digit numbers. For instance, mul(44,46) multiplies 44 by 46 to get a result of 2024. Similarly, mul(123,4) would multiply 123 by 4.
+
+// However, because the program's memory has been corrupted, there are also many invalid characters that should be ignored, even if they look like part of a mul instruction. Sequences like mul(4*, mul(6,9!, ?(12,34), or mul ( 2 , 4 ) do nothing.
+
+// For example, consider the following section of corrupted memory:
+
+// xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+// Only the four highlighted sections are real mul instructions. Adding up the result of each instruction produces 161 (2*4 + 5*5 + 11*8 + 8*5).
+
+// Scan the corrupted memory for uncorrupted mul instructions. What do you get if you add up all of the results of the multiplications?
+
+// --- Part Two ---
+// As you scan through the corrupted memory, you notice that some of the conditional statements are also still intact. If you handle some of the uncorrupted conditional statements in the program, you might be able to get an even more accurate result.
+
+// There are two new instructions you'll need to handle:
+
+// The do() instruction enables future mul instructions.
+// The don't() instruction disables future mul instructions.
+// Only the most recent do() or don't() instruction applies. At the beginning of the program, mul instructions are enabled.
+
+// For example:
+
+// xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+// This corrupted memory is similar to the example from before, but this time the mul(5,5) and mul(11,8) instructions are disabled because there is a don't() instruction before them. The other mul instructions function normally, including the one at the end that gets re-enabled by a do() instruction.
+
+// This time, the sum of the results is 48 (2*4 + 8*5).
+
+// Handle the new instructions; what do you get if you add up all of the results of just the enabled multiplications?
+
+
 fn main() {
-    println!("Hello, world!");
+    let text = get_text();
+
+    let parsed_text: Vec<(i32, i32)> = parse_text_regex(&text);
+    let mut total = 0;
+    parsed_text.iter().for_each(|(a, b)| {
+        total += a * b;
+    });
+    println!("The total is: {}", total);
+
+    let parsed_text_switch = parse_text_switch(&text);
+    let mut total_switch = 0;
+    parsed_text_switch.iter().for_each(|(a, b)| {
+        total_switch += a * b;
+    });
+    println!("The total is: {}", total_switch);
 }
+
+fn get_text() -> String {
+    let mut file = File::open("input.txt").expect("file not found");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("something went wrong reading the file");
+
+    contents
+}
+
+fn parse_text_regex(text: &String) -> Vec<(i32, i32)> {
+    let mut vec: Vec<(i32, i32)> = Vec::new();
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    for cap in re.captures_iter(&text) {
+        let num1 = cap[1].parse::<i32>().unwrap();
+        let num2 = cap[2].parse::<i32>().unwrap();
+        vec.push((num1, num2));
+    }
+    vec
+}
+
+fn parse_text_switch(text: &String) -> Vec<(i32, i32)> {
+    let mut vec: Vec<(i32, i32)> = Vec::new();
+    let re: Regex = Regex::new(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)").unwrap();
+    let mut enabled = true;
+    for cap in re.captures_iter(&text) {
+        if cap[0] == *"do()" {
+            enabled = true;
+        } else if cap[0] == *"don't()" {
+            enabled = false;
+        } else {
+            if enabled {
+                let num1 = cap[1].parse::<i32>().unwrap();
+                let num2 = cap[2].parse::<i32>().unwrap();
+                vec.push((num1, num2));
+            }
+        }
+    }
+    vec
+}
+
+
+// original naive solution -- regex is a lot better, but potentially less performant. Will need to test.
+
+// fn parse_text(text: String) -> Vec<(i32, i32)> {
+//     // let valid_chars = "mul(,)".chars();
+//     let mut valid_chars = HashMap::new();
+//     valid_chars.insert('m', true);
+//     valid_chars.insert('u', true);
+//     valid_chars.insert('l', true);
+//     valid_chars.insert('(', true);
+//     valid_chars.insert(',', true);
+//     valid_chars.insert(')', true);
+//     valid_chars.insert('d', true);
+//     valid_chars.insert('o', true);
+//     valid_chars.insert('n', true);
+//     valid_chars.insert('t', true);
+//     valid_chars.insert('\'', true);
+//     valid_chars.insert('0', true);
+//     valid_chars.insert('1', true);
+//     valid_chars.insert('2', true);
+//     valid_chars.insert('3', true);
+//     valid_chars.insert('4', true);
+//     valid_chars.insert('5', true);
+//     valid_chars.insert('6', true);
+//     valid_chars.insert('7', true);
+//     valid_chars.insert('8', true);
+//     valid_chars.insert('9', true);
+//     let mut vec: Vec<(i32, i32)> = Vec::new();
+//     let mut currentString = String::new();
+//     for c in text.chars() {
+//         if valid_chars.get(&c) == None {
+//             currentString = "".to_string();
+//             continue;
+//         } else {
+//             match currentString {
+
+//             }
+//             currentString.push(c);
+//         }
+//     }
+
+//     // // how do i check whether a char is valid? I could use a regex, but that would be slow
+//     // // i could do a hashmap, which would be faster
+//     // text.iter().for_each(|char| {
+
+//     // })
+//     // text.split("\n").for_each(|line| {
+//     //     vec.push(line.to_string());
+//     // });
+//     vec
+// }
